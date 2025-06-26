9238bf672a8cba9c4ce19b516f41112265c8259b
ojas wane <ojaswane79@gmail.com>
2025-06-26 22:34:42 +0530

    blog app

---
+++ DailyBlog/src/appwrite/auth.js
@@ -0,0 +1,71 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+
+export class Authentification {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+
+        this.account = new Account
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                //return to the login page
+                return this.login({ email, password })
+            } else {
+                return userAccount;
+            }
+
+        } catch (error) {
+            console.error("Appwrite Create Account Error:", error);
+            throw new Error("Failed to create account. Please try again.");
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            const logedin = await this.account.createEmailSession(email, password);
+            return logedin;
+        }
+
+        catch (error) {
+            console.error("Appwrite login Error:", error);
+            throw new Error("Failed to login. Please try again.");
+        }
+    }
+
+    async logout() {
+        try {
+            const logedout = await this.account.deleteSessions();
+            return logedout;
+        }
+
+        catch (error) {
+            console.error("Appwrite logout Error:", error);
+            throw new Error("Failed to log out. Please try again.");
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            const user = await this.account.get();
+            return user
+        }
+        catch (error) {
+            console.error("Appwrite Error:", error);
+            throw new Error("Failed to get current user. Please try again.");
+        }
+    }
+}
+
+const authentification = new Authentification()
+
+export default authentification
---
+++ DailyBlog/src/conf-forDatabase/conf.js
@@ -0,0 +1,78 @@
+import config from "../config/config";
+import { Client, Storage, ID, Databases, Query } from "appwrite";
+
+export class Services {
+    client = new Client;
+    databases;
+    storage;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+
+        this.databases = new Databases;
+        this.storage = new Storage;
+
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabasId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        }
+        catch (error) {
+            console.error("Failed to create post because : ", error);
+            throw new Error("Failed to create Post. Please try again.");
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.storage.createFile(
+                config.appwriteDatabasId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                })
+        }
+
+        catch (error) {
+            console.error("Failed to update post because : ", error);
+            throw new Error("Failed to update post. Please try again.");
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabasId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true;
+        }
+        catch(error) {
+            console.error("Failed to delete post because : ", error);
+            throw new Error("Failed to delete post. Please try again.");
+        }
+
+    }
+}
+
+const services = new Services();
+export default services
